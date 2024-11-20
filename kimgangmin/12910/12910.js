356583f51879f50c45d7ecb4a43bf07ca508ba03
Dobbymin <gmin990624@gmail.com>
2024-11-20 23:02:48 +0900

    solve: 나누어 떨어지는 숫자 배열

---
+++ kimgangmin/12910/12910.js
@@ -0,0 +1,16 @@
+function solution(arr, divisor) {
+	var answer = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] % divisor === 0) {
+			answer.push(arr[i]);
+		}
+	}
+	if (answer.length === 0) {
+		answer.push(-1);
+	} else {
+		answer.sort((a, b) => a - b);
+	}
+
+	return answer;
+}
